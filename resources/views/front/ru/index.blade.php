46fb639de9614261b425f24cd22469391464564a
MrJoker1517 <jas.884@mail.ru>
2022-09-08 14:13:38 +0500

    Main file

--- resources/views/front/ru/index.blade.php
+++ resources/views/front/ru/index.blade.php
@@ -6,7 +6,7 @@
      position: absolute;
     z-index: 9999;
   }
-</style>  
+</style>
 @endsection
 
 @section('content')
@@ -231,7 +231,7 @@
 
     <section>
         <div class="container">
-            <img src="images/AST04894.JPG" class="w-100 rounded" alt="image">
+            <img src="images/AST04894.jpg" class="w-100 rounded" alt="image">
         </div>
     </section>
 
