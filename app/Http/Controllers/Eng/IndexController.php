ee06ed6939bee7c12e091536dfd270c249300b0f
MrJoker1517 <jas.884@mail.ru>
2022-09-08 14:03:57 +0500

    update

--- app/Http/Controllers/Eng/IndexController.php
+++ app/Http/Controllers/Eng/IndexController.php
@@ -8,34 +8,34 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index(Request $request){
-    	return view('front.ru.index', compact('request'));
+    	return view('front.eng.index', compact('request'));
     }
 
     public function job(){
-        return view('front.ru.job');
+        return view('front.eng.job');
     }
 
     public function courses(){
-        return view('front.ru.courses');
+        return view('front.eng.courses');
     }
 
     public function courses_in(){
-        return view('front.ru.courses_in');
+        return view('front.eng.courses_in');
     }
 
     public function gallery(){
-        return view('front.ru.gallery');
+        return view('front.eng.gallery');
     }
 
     public function about_us(){
-        return view('front.ru.about_us');
+        return view('front.eng.about_us');
     }
 
     public function news(){
-        return view('front.ru.news');
+        return view('front.eng.news');
     }
 
     public function news_pages(){
-        return view('front.ru.news_pages');
+        return view('front.eng.news_pages');
     }
 }
--- app/Http/Controllers/Uz/IndexController.php
+++ app/Http/Controllers/Uz/IndexController.php
@@ -8,19 +8,19 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index(Request $request){
-    	return view('front.ru.index', compact('request'));
+    	return view('front.uz.index', compact('request'));
     }
 
     public function job(){
-        return view('front.ru.job');
+        return view('front.uz.job');
     }
 
     public function courses(){
-        return view('front.ru.courses');
+        return view('front.uz.courses');
     }
 
     public function courses_in(){
-        return view('front.ru.courses_in');
+        return view('front.uz.courses_in');
     }
 
     public function gallery(){
@@ -28,15 +28,15 @@ class IndexController extends Controller
     }
 
     public function about_us(){
-        return view('front.ru.about_us');
+        return view('front.uz.about_us');
     }
 
     public function news(){
-        return view('front.ru.news');
+        return view('front.uz.news');
     }
 
     public function news_pages(){
-        return view('front.ru.news_pages');
+        return view('front.uz.news_pages');
     }
 
 }
